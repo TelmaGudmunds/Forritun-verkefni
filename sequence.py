3d17a5994da5206a826883bb65ede9caa278b744
Telma Guðmundsdóttir <telmagudmundsdottir99@gmail.com>
2020-09-03 10:22:45 +0000

    Algorimth written for sequence

---
+++ sequence.py
@@ -0,0 +1,6 @@
+# Get input from user (int)
+
+# Make an algorithm that calculates the sequence:; 1, 2, 3, 6, 11, 20, 37, ...
+# Next number in sequence is the sum of the previous three numbers
+
+# Print the sequence
